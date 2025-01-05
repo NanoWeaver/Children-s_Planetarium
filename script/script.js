42605622a9d9ef755cc6a25289b51eff1f78fd07
Михаил <108208341+NanoWeaver@users.noreply.github.com>
2025-01-05 12:42:36 +0700

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,45 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.gallery__swiper-pagination',
+        type: 'fraction',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.gallery__button--next',
+        prevEl: '.gallery__button--prev',
+    },
+});
+const paginationCurrent = document.querySelector('.swiper-pagination-current');
+const paginationTotal = document.querySelector('.swiper-pagination-total');
+const buttonNext = document.querySelector('.gallery__button--next')
+const buttonPrev = document.querySelector('.gallery__button--prev')
+let cooldown = false
+
+function paginationTransform() { // Добавление нуля перед числом 
+    if (paginationTotal.textContent < 10 && paginationTotal.textContent[0] != 0) {
+        paginationTotal.textContent = '0' + paginationTotal.textContent;
+    }
+    if (paginationCurrent.textContent < 10  && paginationCurrent.textContent[0] != 0) {
+        paginationCurrent.textContent = '0' + paginationCurrent.textContent;
+    }
+}
+
+window.addEventListener('load', function () {
+    setTimeout(function () {
+        paginationTransform(buttonNext)
+    } , 0)
+})
+
+buttonNext.addEventListener('click' , () => {
+    paginationTransform(buttonNext)
+})
+
+buttonPrev.addEventListener('click' , () => {
+    paginationTransform(buttonPrev)
+})
